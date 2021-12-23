c5e77cc916684bd8e4c05dbec8872faa01513135
prajjwal singh <93024165+prajjwal1947@users.noreply.github.com>
2021-12-23 12:34:22 +0530

    first

---
+++ src/components/Result.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Result({ result, openPopup ,arr}) {
+	
+
+	
+	return (
+		
+		<>
+      
+		
+		<div className="result" onClick={() => openPopup(result.imdbID)}>
+			<img src={result.Poster} />
+			<h3>{result.Title}</h3>
+			
+		</div>
+		
+		</>
+
+	)
+}
+
+export default Result
+
