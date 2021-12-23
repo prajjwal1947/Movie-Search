9025013b1a78b915e12cbaf3dbe82a44db3e23b6
prajjwal singh <93024165+prajjwal1947@users.noreply.github.com>
2021-12-23 12:40:03 +0530

    Updates

---
+++ precache-manifest.78cafe32a6ca2b2663dd3b651f180a10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e75ce2fa60614839346a7f83c6b02688",
+    "url": "/index.html"
+  },
+  {
+    "revision": "859b8baaf16bec339951",
+    "url": "/static/css/main.27be6563.chunk.css"
+  },
+  {
+    "revision": "fe4dec88b4311d4cc60d",
+    "url": "/static/js/2.fd08b29f.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.fd08b29f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "859b8baaf16bec339951",
+    "url": "/static/js/main.0b1a67f4.chunk.js"
+  },
+  {
+    "revision": "9f97e5952bc8b13ef682",
+    "url": "/static/js/runtime-main.e65ce676.js"
+  }
+]);
